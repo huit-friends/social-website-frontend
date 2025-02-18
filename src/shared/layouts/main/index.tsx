41bd6a373a3c02158e4f1003c4a61f0f2b05f580
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-02-18 07:21:23 +0700

    feat(components): add left sidebar, right side bar

--- src/shared/layouts/main/index.tsx
+++ src/shared/layouts/main/index.tsx
@@ -1,17 +1,20 @@
-/* import { Footer } from './footer'; */
+// MainLayout.tsx
 import { Header } from './header';
 import { LayoutProps } from '@/models/common';
-import { ModeToggle } from '@/components/toggle-theme';
-
+import { Sidebar } from './sidebar';
+import { RightSidebar } from './right-sidebar';
 
 export function MainLayout({ children }: LayoutProps) {
-    return (
-        <div className='relative h-screen'>
-            <Header />
-            {children}
-            <div className='absolute bottom-14 left-5'>
-                <ModeToggle />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <div className="flex flex-1 flex-col lg:flex-row">
+        <Sidebar className="lg:w-1/5 lg:block hidden" />
+        <main className="flex-1 ml-0 lg:ml-[20%] mr-0 lg:mr-[20%] overflow-y-auto px-4 mt-20">
+          {children}
+        </main>
+        <RightSidebar className="lg:w-1/5 lg:block hidden" />
+      </div>
+    </div>
+  );
 }
